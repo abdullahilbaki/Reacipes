5083f88421bab21ac72c9e6d01d99d9457e39b0f
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-04-09 03:14:02 +0600

    added favorite button

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -20,14 +20,47 @@ const Navbar = () => {
           </div>
         </div>
 
-        <div className="flex items-center sm:gap-4 md:gap-6 lg:gap-8">
+        <div className="flex items-center gap-4 md:gap-6 lg:gap-8">
           {/* Go to reacipes section  */}
-          <a
-            href="#recipes"
-            className="hidden sm:block text-lg font-semibold  hover:underline"
-          >
+          <a href="#recipes" className="hidden sm:block text-lg font-semibold  hover:underline">
             Recipes
           </a>
+
+          {/* Favorites  */}
+          <div className="dropdown dropdown-start hidden sm:block">
+            <div tabIndex="0" role="button" className="btn btn-ghost btn-circle">
+              <div className="indicator">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth="2.5"
+                  stroke="currentColor"
+                  className="size-[1.7em]"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z"
+                  />
+                </svg>
+
+                <span className="indicator-item">8</span>
+              </div>
+            </div>
+            <div
+              tabIndex="0"
+              className="card card-compact dropdown-content bg-base-100 z-1 mt-3 w-52 shadow"
+            >
+              <div className="card-body">
+                <span className="text-lg font-bold">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">View cart</button>
+                </div>
+              </div>
+            </div>
+          </div>
 
           {/* Search box */}
           <label className="input w-auto">
--- src/components/recipes/Recipes.jsx
+++ src/components/recipes/Recipes.jsx
@@ -1,69 +1,100 @@
-import React, { Suspense, use } from "react";
+import { faHeart, faTag } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { Suspense, use, useState } from "react";
 
 const fetchRecipes = async () => {
   const response = await fetch("https://dummyjson.com/recipes");
-  console.log("Response received:", response);
   if (!response.ok) {
-    console.error("Fetch error:", response.status);
     throw new Error("Failed to fetch recipes.");
   }
   const data = await response.json();
-  console.log("Data parsed:", data);
   return data.recipes;
 };
 
 const recipePromise = fetchRecipes();
 const RecipeList = () => {
   const recipes = use(recipePromise);
+  const [Favorites, setFavorites] = useState({});
+
+  const toggleFavorite = (recipeId) => {
+    setFavorites((previous) => ({
+      ...previous,
+      [recipeId]: !previous[recipeId],
+    }));
+  };
+
   return (
     <>
       <div className="p-4 mt-4">
         <div className=" container mx-auto grid sm:grid-cols-2 lg:grid-cols-3 gap-4">
-          {recipes.map((recipe) => (
-            <div key={recipe.id} className="card bg-base-100 shadow-sm">
-              <figure>
-                <img src={recipe.image} alt={recipe.name} />
-              </figure>
-              <div className="card-body flex ">
-                <div className="flex items-center gap-2">
-                  {recipe.difficulty === "Easy" ? (
-                    <div className="badge badge-soft badge-info">
-                      {recipe.difficulty}
+          {recipes.map((recipe) => {
+            const isFavorite = Favorites[recipe.id] || false;
+            return (
+              <div
+                key={recipe.id}
+                className="card bg-base-100 shadow-sm cursor-pointer"
+              >
+                <figure className="relative">
+                  <img
+                    src={recipe.image}
+                    alt={recipe.name}
+                    className="transition-transform duration-300 transform hover:scale-105 focus:scale-105"
+                  />
+                  <FontAwesomeIcon
+                    onClick={() => toggleFavorite(recipe.id)}
+                    icon={faHeart}
+                    className={`${
+                      isFavorite ? "text-red-500" : "text-gray-300"
+                    } absolute text-3xl top-[5%] right-[5%] transition-transform duration-300 transform hover:scale-125 active:scale-110 drop-shadow-lg`}
+                  />
+                </figure>
+                <div className="card-body flex ">
+                  <div className="flex items-center gap-2">
+                    {recipe.difficulty === "Easy" ? (
+                      <div className="badge badge-soft badge-info">
+                        {recipe.difficulty}
+                      </div>
+                    ) : (
+                      <div className="badge badge-soft badge-accent">
+                        {recipe.difficulty}
+                      </div>
+                    )}
+                    <div className="badge badge-secondary">
+                      {recipe.cuisine}
                     </div>
-                  ) : (
-                    <div className="badge badge-soft badge-accent">
-                      {recipe.difficulty}
-                    </div>
-                  )}
-                  <div className="badge badge-secondary">{recipe.cuisine}</div>
-                </div>
-                <h2 className="card-title">{recipe.name}</h2>
-
-                {/* rating */}
-                <div className="flex gap-2 items-center">
-                  <div className="rating">
-                    <div className="mask mask-star" aria-label="1 star"></div>
-                    <div className="mask mask-star" aria-label="2 star"></div>
-                    <div className="mask mask-star" aria-label="3 star"></div>
-                    <div
-                      className="mask mask-star"
-                      aria-label="4 star"
-                      aria-current="true"
-                    ></div>
-                    <div className="mask mask-star" aria-label="5 star"></div>
                   </div>
-                  <p className="text-lg font-medium">{recipe.rating}</p>
-                </div>
-                <p>Based on {recipe.reviewCount} reviews</p>
-                <div className="mt-6 card-actions justify-end">
-                  {recipe.tags.length > 0 &&
-                    recipe.tags.map((tag) => (
-                      <div className="badge badge-outline">{tag}</div>
-                    ))}
+                  <h2 className="card-title">{recipe.name}</h2>
+
+                  {/* rating */}
+                  <div className="flex gap-2 items-center">
+                    <div className="rating">
+                      <div className="mask mask-star" aria-label="1 star"></div>
+                      <div className="mask mask-star" aria-label="2 star"></div>
+                      <div className="mask mask-star" aria-label="3 star"></div>
+                      <div
+                        className="mask mask-star"
+                        aria-label="4 star"
+                        aria-current="true"
+                      ></div>
+                      <div className="mask mask-star" aria-label="5 star"></div>
+                    </div>
+                    <p className="text-lg font-medium">{recipe.rating}</p>
+                  </div>
+                  <p>Based on {recipe.reviewCount} reviews</p>
+
+                  <div className="mt-6 card-actions flex items-center gap-2 flex-wrap">
+                    <FontAwesomeIcon icon={faTag} className="text-lg" />
+                    {recipe.tags.length > 0 &&
+                      recipe.tags.map((tag) => (
+                        <div key={tag} className="badge badge-outline">
+                          {tag}
+                        </div>
+                      ))}
+                  </div>
                 </div>
               </div>
-            </div>
-          ))}
+            );
+          })}
         </div>
       </div>
     </>
@@ -79,8 +110,11 @@ const Loading = () => {
 const Recipes = () => {
   return (
     <>
-      <h1 id="recipes" className="pt-40 sm:pt-30 mx-auto block text-center mb-4 sm:mb-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-shadow-md patrick-hand">
-        Explore reacipes
+      <h1
+        id="recipes"
+        className="pt-40 sm:pt-30 mx-auto block text-center mb-4 sm:mb-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-shadow-md patrick-hand"
+      >
+        Explore recipes
       </h1>
 
       <Suspense fallback={<Loading />}>
