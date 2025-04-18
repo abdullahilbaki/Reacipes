040507f78b0970cd8fac2d294bf8e3057a7e61c5
abdullahilbaki <abdullahalbaqi00@gmail.com>
2025-04-18 12:37:39 +0600

    get search string from search box on change

---
+++ src/components/search/Search.jsx
@@ -0,0 +1,50 @@
+import React, { useRef, useState } from "react";
+
+const Search = () => {
+  const [searchStr, setSearchStr] = useState("");
+  const inputRef = useRef(null);
+
+  const handleSearchOnChange = (e) => {
+    setSearchStr(e.target.value);
+  };
+  const handleFocus = () => {
+    window.location.hash = "#recipes";
+  };
+
+  const handleLabelClick = () => {
+    inputRef.current?.focus();
+  };
+  return (
+    <label className="input w-auto" onClick={handleLabelClick}>
+      <svg
+        className="h-[1em] opacity-50"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <g
+          strokeLinejoin="round"
+          strokeLinecap="round"
+          strokeWidth="2.5"
+          fill="none"
+          stroke="currentColor"
+        >
+          <circle cx="11" cy="11" r="8"></circle>
+          <path d="m21 21-4.3-4.3"></path>
+        </g>
+      </svg>
+      <input
+        ref={inputRef}
+        type="search"
+        className="grow"
+        value={searchStr}
+        onChange={handleSearchOnChange}
+        onFocus={handleFocus}
+        placeholder="Search for a recipe"
+      />
+      <kbd className="kbd kbd-sm">⌘</kbd>
+      <kbd className="kbd kbd-sm">K</kbd>
+    </label>
+  );
+};
+
+export default Search;
